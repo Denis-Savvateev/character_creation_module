1b3ac33321f8893e7e3aa152d1c2b8b352fc5f03
Denis Savvateev <densavvateev@yandex.ru>
2023-09-06 21:16:33 +0300

    Save file normaly.

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
-"""This is the main module of game."""
-
 from random import randint
 
-from graphic_arts.start_game_banner import run_screensaver
+DEFAULT_ATTACK = 5
+DEFAULT_DEFENCE = 10
+DEFAULT_STAMINA = 80
 
 
-class Chsrscter():
+class Character:
+    # Новая константа.
+    BRIEF_DESC_CHAR_CLASS = 'отважный любитель приключений'
+    RANGE_VALUE_ATTACK = (1, 3)
+    RANGE_VALUE_DEFENCE = (1, 5)
+    SPECIAL_BUFF = 15
+    SPECIAL_SKILL = 'Удача'
+
     def __init__(self, name):
         self.name = name
 
     def attack(self):
-        return (f'{self.name} нанёс урон противнику '
-                f'равный {5 + randint(3, 5)}')
+        value_attack = DEFAULT_ATTACK + randint(*self.RANGE_VALUE_ATTACK)
+        return (f'{self.name} нанёс противнику урон, равный {value_attack}')
+
+    def defence(self):
+        value_defence = DEFAULT_DEFENCE + randint(*self.RANGE_VALUE_DEFENCE)
+        return (f'{self.name} блокировал {value_defence} ед. урона')
+
+    def special(self):
+        return (f'{self.name} применил специальное умение '
+                f'"{self.SPECIAL_SKILL} {self.SPECIAL_BUFF}"')
+
+    # Новый метод базового класса.
+    def __str__(self):
+        return f'{self.__class__.__name__} - {self.BRIEF_DESC_CHAR_CLASS}.'
+    ...
+
+
+class Warrior(Character):
+    ...
+
+
+class Mage(Character):
+    ...
+
+
+class Healer(Character):
+    ...
